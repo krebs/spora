0d573b15b2241b216fa48c7b7ecc3711a495bf25
lassulus <git@lassul.us>
2024-03-18 15:56:47 +0100

    mycelium: get rid of flatten

--- flake.nix
+++ flake.nix
@@ -85,7 +85,7 @@
               RestrictRealtime = true;
               SystemCallArchitectures = "native";
               SystemCallFilter = [ "@system-service" "~@privileged @keyring" ];
-              ExecStart = lib.concatStringsSep " " (lib.flatten [
+              ExecStart = lib.concatStringsSep " " ([
                 (lib.getExe cfg.package)
                 (if (cfg.keyFile != null) then
                   "--key-file \${CREDENTIALS_DIRECTORY}/keyfile" else
@@ -93,39 +93,38 @@
                 )
                 "--debug"
                 "--tun-name" "mycelium"
-                "--peers" cfg.peers
-                (lib.optionals cfg.addHostedNodes [
-                  "tcp://188.40.132.242:9651" # DE 01
-                  "tcp://[2a01:4f8:221:1e0b::2]:9651"
-                  "quic://188.40.132.242:9651"
-                  "quic://[2a01:4f8:221:1e0b::2]:9651"
+                "--peers"
+              ] ++ cfg.peers ++ (lib.optionals cfg.addHostedNodes [
+                "tcp://188.40.132.242:9651" # DE 01
+                "tcp://[2a01:4f8:221:1e0b::2]:9651"
+                "quic://188.40.132.242:9651"
+                "quic://[2a01:4f8:221:1e0b::2]:9651"
 
-                  "tcp://136.243.47.186:9651" # DE 02
-                  "tcp://[2a01:4f8:212:fa6::2]:9651"
-                  "quic://136.243.47.186:9651"
-                  "quic://[2a01:4f8:212:fa6::2]:9651"
+                "tcp://136.243.47.186:9651" # DE 02
+                "tcp://[2a01:4f8:212:fa6::2]:9651"
+                "quic://136.243.47.186:9651"
+                "quic://[2a01:4f8:212:fa6::2]:9651"
 
-                  "tcp://185.69.166.7:9651" # BE 03
-                  "tcp://[2a02:1802:5e:0:8478:51ff:fee2:3331]:9651"
-                  "quic://185.69.166.7:9651"
-                  "quic://[2a02:1802:5e:0:8478:51ff:fee2:3331]:9651"
+                "tcp://185.69.166.7:9651" # BE 03
+                "tcp://[2a02:1802:5e:0:8478:51ff:fee2:3331]:9651"
+                "quic://185.69.166.7:9651"
+                "quic://[2a02:1802:5e:0:8478:51ff:fee2:3331]:9651"
 
-                  "tcp://185.69.166.8:9651" # BE 04
-                  "tcp://[2a02:1802:5e:0:8c9e:7dff:fec9:f0d2]:9651"
-                  "quic://185.69.166.8:9651"
-                  "quic://[2a02:1802:5e:0:8c9e:7dff:fec9:f0d2]:9651"
+                "tcp://185.69.166.8:9651" # BE 04
+                "tcp://[2a02:1802:5e:0:8c9e:7dff:fec9:f0d2]:9651"
+                "quic://185.69.166.8:9651"
+                "quic://[2a02:1802:5e:0:8c9e:7dff:fec9:f0d2]:9651"
 
-                  "tcp://65.21.231.58:9651" # FI 05
-                  "tcp://[2a01:4f9:6a:1dc5::2]:9651"
-                  "quic://65.21.231.58:9651"
-                  "quic://[2a01:4f9:6a:1dc5::2]:9651"
+                "tcp://65.21.231.58:9651" # FI 05
+                "tcp://[2a01:4f9:6a:1dc5::2]:9651"
+                "quic://65.21.231.58:9651"
+                "quic://[2a01:4f9:6a:1dc5::2]:9651"
 
-                  "tcp://65.109.18.113:9651" # FI 06
-                  "tcp://[2a01:4f9:5a:1042::2]:9651"
-                  "quic://65.109.18.113:9651"
-                  "quic://[2a01:4f9:5a:1042::2]:9651"
-                ])
-              ]);
+                "tcp://65.109.18.113:9651" # FI 06
+                "tcp://[2a01:4f9:5a:1042::2]:9651"
+                "quic://65.109.18.113:9651"
+                "quic://[2a01:4f9:5a:1042::2]:9651"
+              ]));
               Restart = "always";
               RestartSec = 5;
               TimeoutStopSec = 5;
