00bec860376902429624f5267457c65fcab73ad5
lassulus <git@lassul.us>
2024-03-18 12:30:21 +0100

    mycelium: add addUpstreamPeers option

--- flake.nix
+++ flake.nix
@@ -48,6 +48,13 @@
             default = pkgs.mycelium;
             description = "The mycelium package to use";
           };
+          addUpstreamPeers = lib.mkOption {
+            type = lib.types.bool;
+            default = true;
+            description = ''
+              add the upstream peers from https://github.com/threefoldtech/mycelium#hosted-public-nodes
+            '';
+          };
         };
         config = lib.mkIf cfg.enable {
           networking.firewall.allowedTCPPorts = lib.optionals cfg.openFirewall [ 9651 ];
@@ -64,9 +71,40 @@
               ExecStart = lib.concatStringsSep " " (lib.flatten [
                 (lib.getExe cfg.package)
                 (lib.optionals (cfg.keyFile != null) "--key-file ${cfg.keyFile}")
+                "--debug"
                 "--tun-name" "myc"
                 "--peers" cfg.peers
-                "--debug"
+                (lib.optionals cfg.addUpstreamPeers [
+                  "tcp://188.40.132.242:9651" # DE 01
+                  "tcp://[2a01:4f8:221:1e0b::2]:9651"
+                  "quic://188.40.132.242:9651"
+                  "quic://[2a01:4f8:221:1e0b::2]:9651"
+
+                  "tcp://136.243.47.186:9651" # DE 02
+                  "tcp://[2a01:4f8:212:fa6::2]:9651"
+                  "quic://136.243.47.186:9651"
+                  "quic://[2a01:4f8:212:fa6::2]:9651"
+
+                  "tcp://185.69.166.7:9651" # BE 03
+                  "tcp://[2a02:1802:5e:0:8478:51ff:fee2:3331]:9651"
+                  "quic://185.69.166.7:9651"
+                  "quic://[2a02:1802:5e:0:8478:51ff:fee2:3331]:9651"
+
+                  "tcp://185.69.166.8:9651" # BE 04
+                  "tcp://[2a02:1802:5e:0:8c9e:7dff:fec9:f0d2]:9651"
+                  "quic://185.69.166.8:9651"
+                  "quic://[2a02:1802:5e:0:8c9e:7dff:fec9:f0d2]:9651"
+
+                  "tcp://65.21.231.58:9651" # FI 05
+                  "tcp://[2a01:4f9:6a:1dc5::2]:9651"
+                  "quic://65.21.231.58:9651"
+                  "quic://[2a01:4f9:6a:1dc5::2]:9651"
+
+                  "tcp://65.109.18.113:9651" # FI 06
+                  "tcp://[2a01:4f9:5a:1042::2]:9651"
+                  "quic://65.109.18.113:9651"
+                  "quic://[2a01:4f9:5a:1042::2]:9651"
+                ])
               ]);
               Restart = "always";
               RestartSec = 2;
@@ -89,38 +127,7 @@
           peers = let
             hostsWithIps = lib.filterAttrs (name: host: lib.hasAttr "public_endpoints" host) self.lib.hosts;
           in
-            lib.flatten (map (host: host.public_endpoints) (lib.attrValues hostsWithIps)) ++ [
-              ## official servers
-              "tcp://188.40.132.242:9651" # DE 01
-              "tcp://[2a01:4f8:221:1e0b::2]:9651"
-              "quic://188.40.132.242:9651"
-              "quic://[2a01:4f8:221:1e0b::2]:9651"
-
-              "tcp://136.243.47.186:9651" # DE 02
-              "tcp://[2a01:4f8:212:fa6::2]:9651"
-              "quic://136.243.47.186:9651"
-              "quic://[2a01:4f8:212:fa6::2]:9651"
-
-              "tcp://185.69.166.7:9651" # BE 03
-              "tcp://[2a02:1802:5e:0:8478:51ff:fee2:3331]:9651"
-              "quic://185.69.166.7:9651"
-              "quic://[2a02:1802:5e:0:8478:51ff:fee2:3331]:9651"
-
-              "tcp://185.69.166.8:9651" # BE 04
-              "tcp://[2a02:1802:5e:0:8c9e:7dff:fec9:f0d2]:9651"
-              "quic://185.69.166.8:9651"
-              "quic://[2a02:1802:5e:0:8c9e:7dff:fec9:f0d2]:9651"
-
-              "tcp://65.21.231.58:9651" # FI 05
-              "tcp://[2a01:4f9:6a:1dc5::2]:9651"
-              "quic://65.21.231.58:9651"
-              "quic://[2a01:4f9:6a:1dc5::2]:9651"
-
-              "tcp://65.109.18.113:9651" # FI 06
-              "tcp://[2a01:4f9:5a:1042::2]:9651"
-              "quic://65.109.18.113:9651"
-              "quic://[2a01:4f9:5a:1042::2]:9651"
-            ];
+            lib.flatten (map (host: host.public_endpoints) (lib.attrValues hostsWithIps));
         };
       };
 
